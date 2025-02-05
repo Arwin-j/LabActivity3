ff6ba6484daa0e4fa439a0ae2ddf09bf48008c8e
Arwin-j <tuq56068@temple.edu>
2025-02-05 15:50:25 -0500

    Added Feature

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -20,7 +20,13 @@ class MainActivity : AppCompatActivity() {
 
         
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            if((findViewById<EditText>(R.id.nameEditText).text.toString()).isEmpty()){
+                findViewById<EditText>(R.id.nameEditText).error = "Please Enter your name"
+            }
+            else{
+                displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            }
+
         }
 
 
